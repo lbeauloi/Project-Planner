66285e8a332c7a177a810b95029dc6c672f76a77
Lucas Beauloi <l.beauloi@gmail.com>
2023-10-03 12:16:50 +0200

    génération carte + design de base

---
+++ script.js
@@ -0,0 +1,16 @@
+function createCards() {
+    let article = document.createElement("article");
+    article.innerHTML = `
+      <form class="carte">
+          <input type="text" id="title" class="title" placeholder="Enter your title"></input>
+          <textarea id="text" name="message" class="textInput" placeholder="Enter your text"></textarea>
+      </form>`;
+  
+    let section = document.getElementById("table");
+    section.appendChild(article);
+  }
+  
+  let test = document.getElementById("test");
+  test.addEventListener("click", createCards);
+  
+  
