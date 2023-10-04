896407dfdaeed850b5f55b8081580da1f8bca23a
Lucas Beauloi <l.beauloi@gmail.com>
2023-10-04 12:11:20 +0200

    date ok

--- assets/script/date.js
+++ assets/script/date.js
@@ -1,23 +1,3 @@
-// let dateInput = document.getElementById("userdateInput");
-// let resultElement = document.getElementById("result");
-
-// function daysLeft() {
-//   let userDate = new Date(dateInput.value);
-//   let today = new Date();
-//   const differenceInDays = Math.floor(
-//     (userDate - today) / (1000 * 60 * 60 * 24)
-//   ); // arrondir en jour
-//   if (differenceInDays === 0) {
-//     resultElement.textContent = "End date is today!";
-//   } else if (differenceInDays > 0) {
-//     resultElement.textContent = `${differenceInDays} days remaining.`;
-//   } else {
-//     resultElement.textContent = `The selected date has passed ${-differenceInDays} days.`;
-//   }
-// }
-
-// dateInput.addEventListener("input", daysLeft);
-
 let dateInputs = document.querySelectorAll("input[type='date']");
 let resultElements = document.querySelectorAll(".carte p");
 
@@ -25,7 +5,7 @@ function daysLeft(index) {
   let userDate = new Date(dateInputs[index].value);
   let today = new Date();
   const differenceInDays = Math.floor(
-    (userDate - today) / (1000 * 60 * 60 * 24) //passe les milliseconde en jour
+    (userDate - today) / (1000 * 60 * 60 * 24) //passer les milliseconde en jour
   );
   if (differenceInDays === 0) {
     resultElements[index].textContent = "End date is today!";
