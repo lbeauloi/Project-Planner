513183a93a6a8598792b75edcef8c009f53a1209
Mathias Barbier <mathbarbier07@gmail.com>
2023-10-04 12:26:52 +0200

    update kuskusklan

--- assets/script/card.js
+++ assets/script/card.js
@@ -1,10 +1,10 @@
 function createCards() {
   let article = document.createElement("article");
   article.innerHTML = `
-      <form class="carte">
+      <form class="carte" draggable="true" ondragstart="drag(event)">
           <input type="text" id="title" class="title" placeholder="Enter your title"></input>
           <textarea id="text" name="message" class="textInput" placeholder="Enter your text"></textarea>
-          <label for="dateInput">Task end date:</label>
+          <label>Task end date:</label>
           <input type="date" id="userdateInput">
           <p id="result"></p> 
       </form>`;
@@ -22,16 +22,29 @@ let resultElement = document.getElementById("result");
 function daysLeft() {
   let userDate = new Date(dateInput.value);
   let today = new Date();
-  const differenceInDays = Math.floor(
-    (userDate - today) / (1000 * 60 * 60 * 24)
-  ); // arrondir en jour
+  const differenceInDays = calcTime(userDate, today);
+  // arrondir en jour
   if (differenceInDays === 0) {
     resultElement.textContent = "End date is today!";
   } else if (differenceInDays > 0) {
-    resultElement.textContent = `${differenceInDays} days remaining.`;
+    resultElement.textContent = differenceInDays.jours + " days remaining.";
   } else {
-    resultElement.textContent = `The selected date has passed ${-differenceInDays} days.`;
+    resultElement.textContent = `The selected date has passed ${differenceInDays.jours} days.`;
   }
 }
 
-dateInput.addEventListener("input", daysLeft);
+function calcTime (startDate, endDate) {
+  const difference = endDate - startDate;
+  let secondes = Math.floor(difference / 1000);
+  let minutes = Math.floor(secondes / 60);
+  let heures = Math.floor(minutes / 60);
+  let jours = Math.floor(heures / 24);
+  
+  return{
+    jours: jours,
+    heures: heures % 24,
+    minutes: minutes % 60,
+    secondes: secondes % 60
+  }
+}
+
